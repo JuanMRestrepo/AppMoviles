dac3ccf17196d3fdf8ae17fc566172f73d383caf
jhonandresdiazcano <diazandreslm10@gmail.com>
2025-04-05 15:25:36 -0500

    init

--- Unieventos/app/src/main/java/com/unieventos/ui/navigation/Navigation.kt
+++ Unieventos/app/src/main/java/com/unieventos/ui/navigation/Navigation.kt
@@ -24,8 +24,8 @@ fun Navigation(){
     Surface {
         NavHost(
             navController = navController,
-            //startDestination = RouteScreen.HomeScreen
-            startDestination = RouteScreen.AdminMenuScreen
+            startDestination = RouteScreen.HomeScreen
+            //startDestination = RouteScreen.AdminMenuScreen
         ) {
             composable <RouteScreen.HomeScreen> {
                 HomeScreen(
